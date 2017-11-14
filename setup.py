ce1a351854cd5684df581b25e4ba6c9d827de0f3
Pablo Camino <pablo.camino@ambient-innovation.com>
2017-11-14 15:28:07 +0100

    Validation bugfix

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="ai-django-fileupload",
-    version="0.0.7",
+    version="0.0.8",
     author=u'Ambient Innovation GmbH',
     author_email='info@ambient-innovation.com',
     packages=find_packages(),
