95d16540a5907bd6e2069316b9683543eea041f5
Pablo Camino <pablo.camino@ambient-innovation.com>
2017-11-16 09:10:14 +0100

    Update setup

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="ai-django-fileupload",
-    version="0.0.9",
+    version="0.0.10",
     author=u'Ambient Innovation GmbH',
     author_email='info@ambient-innovation.com',
     packages=find_packages(),
