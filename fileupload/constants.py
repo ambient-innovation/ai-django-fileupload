ec07c181b01d7c7668b6a379b064b2e73549e519
Felix Schul <felix.schul@ambient-innovation.com>
2018-09-13 08:22:35 +0200

    Fixed path to static default placeholder. It used to refer to the node modules folder, but this is not smart. It leads to problems in production when statuc files are collected, since there is no more node_modules folder then. Instead the node_modules should be included in the staticfiles finder.

--- fileupload/constants.py
+++ fileupload/constants.py
@@ -1,7 +1,7 @@
 from django.contrib.staticfiles.templatetags.staticfiles import static
 
 
-UPLOADER_DEFAULT_THUMBNAIL = static('node_modules/ai-django-fileupload/img/default-thumbnail.png')
+UPLOADER_DEFAULT_THUMBNAIL = static('img/default-thumbnail.png')
 
 UPLOADER_LOGIN_REQUIRED = False
 
